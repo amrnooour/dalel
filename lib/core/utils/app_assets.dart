a7c2da31532d6f280bf0ac77f52a5a2e6dddb74b
amr <amrnooour@gmail.com>
2024-02-11 23:32:08 +0200

    Initial commit

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,10 @@
+abstract class AppAssets{
+  static const String onBoarding1 = "assets/images/onboarding1.png";
+  static const String onBoarding2 = "assets/images/onboarding2.png";
+  static const String onBoarding3 = "assets/images/onboarding3.png";
+  static const String forgotPassword = "assets/images/forgotPassword.png";
+  static const String maskGroup = "assets/images/Mask Group.svg";
+  static const String success = "assets/images/Success.png";
+  static const String vector1 = "assets/images/Vector1.svg";
+  static const String vector2 = "assets/images/Vector2.svg";
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Dalel());
+}
+
+class Dalel extends StatelessWidget {
+  const Dalel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
